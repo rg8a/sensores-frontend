3ae9e8d5de5ddb2a869ac5fffa4745634aad4f41
Ochoagram <ricardogochoa@me.com>
2024-11-18 17:41:36 -0600

    wip

--- app/page.js
+++ app/page.js
@@ -5,25 +5,53 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          className="mx-auto"
+          src="/logoequipo.svg"
           alt="Next.js logo"
-          width={180}
+          width={250}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <p className="text-sm text-center font-[family-name:var(--font-geist-mono)] w-[600px]">Diseño de prototipo de un sistema digital capaz de obtener datos mediante el uso de sensores, procesarlos y depositarlos como información en una plataforma en internet, para su análisis y visualización.</p>
+        <ol className="mx-auto mt-4 list-inside list-decimal text-sm text-center font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
+            Ricardo Gaspar Ochoa -
+            <code className="bg-black/[.05] dark:bg-white/[.06] ml-1 px-1 py-0.5 rounded font-semibold">
+              A00838841
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">
+          Emiliano Enríquez López -
+            <code className="bg-black/[.05] dark:bg-white/[.06] ml-1 px-1 py-0.5 rounded font-semibold">
+            A01174554
+            </code>
+            .
+          </li>
+          <li className="mb-2">
+          Jose Manuel Sanchez Perez -
+            <code className="bg-black/[.05] dark:bg-white/[.06] ml-1 px-1 py-0.5 rounded font-semibold">
+            A01178230
+            </code>
+            .
+          </li>
+          <li className="mb-2">
+            Octavio Ramos Treviño -
+            <code className="bg-black/[.05] dark:bg-white/[.06] ml-1 px-1 py-0.5 rounded font-semibold">
+              A00840145
+            </code>
+            .
+          </li>
+          <li className="mb-2">
+          Edmundo Ruelas Angulo -
+            <code className="bg-black/[.05] dark:bg-white/[.06] ml-1 px-1 py-0.5 rounded font-semibold">
+            A01742824
+            </code>
+            .
+          </li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="mx-auto flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/sensorDatos"
@@ -39,7 +67,7 @@ export default function Home() {
             Ver Datos
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -47,6 +75,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
--- app/sensorDatos/page.js
+++ app/sensorDatos/page.js
@@ -3,6 +3,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import Chart from "chart.js/auto";
 import "chartjs-adapter-date-fns";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function SensorDatosPage() {
   const [data, setData] = useState([]);
@@ -12,8 +14,8 @@ export default function SensorDatosPage() {
   const pieChartRef = useRef(null);
   const chartInstance = useRef(null);
   const pieChartInstance = useRef(null);
-
-  console.log(data);
+  const [showGreaterThan50, setShowGreaterThan50] = useState(true);
+  const [showLessThan50, setShowLessThan50] = useState(true);
 
   useEffect(() => {
     async function fetchSensorData() {
@@ -41,57 +43,75 @@ export default function SensorDatosPage() {
         chartInstance.current.destroy();
       }
 
+      // Filtrar datos según los estados de visibilidad
       const chartData = data
         .filter((item) => item.valor_fotoresistor !== null)
+        .filter((item) =>
+          showGreaterThan50 && showLessThan50
+            ? true
+            : showGreaterThan50
+            ? item.valor_fotoresistor > 50
+            : showLessThan50
+            ? item.valor_fotoresistor <= 50
+            : false
+        )
         .map((item) => ({
           x: new Date(item.tiempo_registro).getTime(),
           y: item.valor_fotoresistor,
           r: 5,
-        }))
-        .sort((a, b) => a.x - b.x);
-
-      chartInstance.current = new Chart(ctx, {
-        type: "bubble",
-        data: {
-          datasets: [
-            {
-              label: "Valores del Fotoresistor",
-              data: chartData,
-              backgroundColor: "rgba(75, 192, 192, 0.2)",
-              borderColor: "rgb(75, 192, 192)",
-            },
-          ],
-        },
-        options: {
-          responsive: true,
-          scales: {
-            x: {
-              type: "time",
-              time: {
-                unit: "hour",
+          backgroundColor:
+            item.valor_fotoresistor < 50
+              ? "rgba(75, 192, 192, 0.5)"
+              : "rgba(192, 75, 75, 0.5)",
+          borderColor:
+            item.valor_fotoresistor < 50
+              ? "rgb(75, 192, 192)"
+              : "rgb(192, 75, 75)",
+        }));
+    
+        chartInstance.current = new Chart(ctx, {
+          type: "bubble",
+          data: {
+            datasets: [
+              {
+                label: "Valores del Fotoresistor",
+                data: chartData,
+                backgroundColor: chartData.map((item) => item.backgroundColor),
+                borderColor: chartData.map((item) => item.borderColor),
               },
+            ],
+          },
+          options: {
+            responsive: true,
+            scales: {
+              x: {
+                type: "time",
+                time: {
+                  unit: "hour",
+                },
+                title: {
+                  display: true,
+                  text: "Tiempo",
+                },
+              },
+              y: {
+                title: {
+                  display: true,
+                  text: "Valor del Fotoresistor",
+                },
+              },
+            },
+            plugins: {
               title: {
                 display: true,
-                text: "Tiempo",
-              },
-            },
-            y: {
-              title: {
-                display: true,
-                text: "Valor del Fotoresistor",
+                text: "Valores del Fotoresistor",
               },
             },
           },
-          plugins: {
-            title: {
-              display: true,
-              text: "Valores del Fotoresistor",
-            },
-          },
-        },
-      });
+        });
+        
     }
-  }, [data]);
+  }, [data, showGreaterThan50, showLessThan50]);
 
   // Pie chart for LED state
   useEffect(() => {
@@ -147,9 +167,19 @@ export default function SensorDatosPage() {
     data.filter((item) => item.valor_fotoresistor !== null).length;
 
   return (
-    <div className="container flex flex-col mx-auto items-center">
+    <>
+    <div className="container flex flex-col mx-auto items-center px-10">
       <h1 className="text-3xl font-bold mt-10">Datos del Sensor Fotoresistor (LDR)</h1>
-      <h2 className="text-xl font-bold mb-10">Equipo 4</h2>
+      <Link href="/" passHref>
+        <Image
+            className="mx-auto my-5 mb-10"
+            src="/logoequipo.svg"
+            alt="Next.js logo"
+            width={200}
+            height={38}
+            priority
+        />
+    </Link>
       <button
         onClick={() => setShowTable(true)}
         className="mb-10 px-4 py-2 w-full rounded-full bg-green-500 text-white font-semibold md:w-[200px]"
@@ -157,24 +187,45 @@ export default function SensorDatosPage() {
         Ver tabla principal
       </button>
 
-      <div className="flex flex-col gap-4 items-start mx-auto lg:flex-row lg:gap-5 mb-10">
+      <div className="flex flex-col gap-4 items-start mx-auto lg:flex-row lg:gap-5 mb-10 lg:px-0">
         <div className="w-full max-w-4xl flex flex-col gap-4">
             <div className="w-full max-w-4xl bg-gray-100 rounded-2xl p-5">
             {data.length > 0 && (
-                <div className="mb-4 text-center flex w-full items-center">
+                <div className="mb-4 text-center flex w-full items-center flex-col">
                     <h3 className="text-3xl text-black font-bold mr-2">Fechas de registros:</h3>
                     <p className="text-2xl font-semibold text-gray-700">
                     <span className="font-bold">
                         {new Date(Math.min(...data.map(d => new Date(d.tiempo_registro)))).toLocaleDateString()} a {new Date(Math.max(...data.map(d => new Date(d.tiempo_registro)))).toLocaleDateString()}
                     </span>
                     </p>
+                    
+                    <div className="flex gap-4 my-4">
+                      <label className="flex items-center gap-2 text-gray-700">
+                        <input
+                          type="checkbox"
+                          checked={showGreaterThan50}
+                          onChange={(e) => setShowGreaterThan50(e.target.checked)}
+                        />
+                        {"Valores > 50"}
+                      </label>
+                      <label className="flex items-center gap-2 text-gray-700">
+                        <input
+                          type="checkbox"
+                          checked={showLessThan50}
+                          onChange={(e) => setShowLessThan50(e.target.checked)}
+                        />
+                        {"Valores <= 50"}
+                      </label>
+                    </div>
+            
+                    {/* Gráfica */}
+                    <canvas
+                      ref={chartRef}
+                      style={{ maxWidth: "600px", margin: "0 auto", width: "100%" }}
+                    />
                 </div>
             )}
 
-            <canvas
-                ref={chartRef}
-                style={{ maxWidth: "600px", margin: "0 auto", width: "100%" }}
-            />
             </div>
 
             <div className="w-full flex gap-4">
@@ -194,15 +245,14 @@ export default function SensorDatosPage() {
                 <div className="w-full max-w-4xl bg-gray-100 rounded-2xl p-5 h-[250px] lg:h-auto">
                     <h3 className="text-xl font-bold text-black">Valores registrados</h3>
                     <p className="text-lg mt-4 text-gray-600">
-                    <strong>Promedio:</strong> <span className="text-2xl font-bold">{averageValue.toFixed(2)}</span>
+                    <strong>Promedio:</strong> <span className="text-3xl font-bold">{averageValue.toFixed(2)}</span>
                     </p>
                 </div>
 
                 <div className="w-full max-w-4xl bg-gray-100 rounded-2xl p-5 h-[250px] lg:h-auto">
                     <h3 className="text-xl font-bold text-black">Tiempo encendido hrs:</h3>
                     <p className="text-lg mt-4 text-gray-600">
-                        <strong>Tiempo encendida hrs:</strong>{" "}
-                        <span className="text-2xl font-bold">
+                        <span className="text-3xl font-bold">
                         {(
                             Object.values(data) // Convertir el objeto en un array
                             .filter((item) => item.modo_operacion === 1) // Filtrar los registros donde el LED está encendido
@@ -290,5 +340,53 @@ export default function SensorDatosPage() {
         </div>
       )}
     </div>
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-10">
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/file.svg"
+              alt="File icon"
+              width={16}
+              height={16}
+            />
+            Documentación
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://lookerstudio.google.com/reporting/10eee32e-c578-4589-ba04-749eeef34dc5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/window.svg"
+              alt="Window icon"
+              width={16}
+              height={16}
+            />
+            Looker Studio →
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://github.com/rg8a/sensores-backend"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+            />
+            Backend Github →
+          </a>
+    </footer>
+    </>
   );
 }
