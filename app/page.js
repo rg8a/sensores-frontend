f7a29fb2decbf15e2b9f54bb5a6439f97b5272aa
Ochoagram <ricardogochoa@me.com>
2024-11-22 16:26:43 -0600

    btn color

--- app/page.js
+++ app/page.js
@@ -52,7 +52,7 @@ export default function Home() {
         </ol>
         <div className="mx-auto flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/dashboard"
             rel="noopener noreferrer"
           >
